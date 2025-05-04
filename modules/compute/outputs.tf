19c8c24cb18816fa6cf27c7d6a49f265a131852d
NajehCh <bencharradanajeh@gmail.com>
2025-05-05 00:14:30 +0100

    nodejs instance

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,10 +1,11 @@
-output "public_ip_jenkins" {
+output "public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 
-output "instance_id_jenkins" {
+output "instance_id" {
   value = aws_instance.jenkins.id
 }
+
 
 output "public_ip_nodejs" {
   value = aws_instance.nodejs_instance.public_ip
