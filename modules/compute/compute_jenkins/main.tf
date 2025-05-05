9f9d8c7cd42f6f096316a8463d340175cf2a400b
NajehCh <bencharradanajeh@gmail.com>
2025-05-05 01:07:44 +0100

    jenkins name

---
+++ modules/compute/compute_jenkins/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "jenkins" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [var.security_group_id]
+    associate_public_ip_address = true
+
+    tags = {
+      Name = var.instance_name
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install -y openjdk-17-jdk
+                curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+                echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+                https://pkg.jenkins.io/debian binary/ | sudo tee \
+                /etc/apt/sources.list.d/jenkins.list > /dev/null
+                sudo apt update -y
+                sudo apt install -y jenkins
+                EOF
+}
